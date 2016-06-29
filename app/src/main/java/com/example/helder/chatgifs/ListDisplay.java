fdc9fefaf9d1b67e134ae29f5c1f40cefb8554e9
Helder <hedie.strokes@gmail.com>
2016-06-28 21:36:41 -0300

    "Upei o trabalho imcompleto para vc colocar a parte do response para os gofs, existe um comentario na class do chatActivity que vai te ajudar a saber onde mandar o json etc

---
+++ app/src/main/java/com/example/helder/chatgifs/ListDisplay.java
@@ -0,0 +1,55 @@
+package com.example.helder.chatgifs;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDisplay extends ArrayAdapter<OneComment> {
+
+private TextView textViewItemMensage;
+private List<OneComment> countries = new ArrayList<OneComment>();
+private LinearLayout wrapperMensageItem;
+
+
+public ListDisplay(Context context, int textViewResourceId) {
+    super(context, textViewResourceId);
+}
+
+public void add(OneComment object) {
+    countries.add(object);
+    super.add(object);
+}
+
+public int getCount() {
+    return this.countries.size();
+}
+
+public OneComment getItem(int index) {
+    return this.countries.get(index);
+}
+
+public View getView(int position, View convertView, ViewGroup parent) {
+    View row = convertView;
+    if (row == null) {
+        LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        row = inflater.inflate(R.layout.activity_listview, parent, false);
+    }
+
+    wrapperMensageItem = (LinearLayout) row.findViewById(R.id.wrapperMensageItem);
+    OneComment coment = getItem(position);
+
+    textViewItemMensage = (TextView) row.findViewById(R.id.textViewItemMensage);
+    textViewItemMensage.setText(coment.comment);
+    textViewItemMensage.setBackgroundResource(coment.left ? R.drawable.buble2 : R.drawable.buble);
+    wrapperMensageItem.setGravity(coment.left ? Gravity.LEFT : Gravity.RIGHT);
+    return row;
+}
+}
---
+++ app/src/main/java/com/example/helder/chatgifs/OneComment.java
@@ -0,0 +1,15 @@
+package com.example.helder.chatgifs;
+
+/**
+ * Created by helder.silva on 06/06/2016.
+ */
+public class OneComment {
+    public boolean left;
+    public String comment;
+
+    public OneComment(boolean left, String comment) {
+        super();
+        this.left = left;
+        this.comment = comment;
+    }
+}
